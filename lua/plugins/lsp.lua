dd5232ce920fa5686fe9d1eca7f129f83a660a55
Nikita-Nick Funk <nikita.nick.funk@gmail.com>
2025-06-09 07:26:36 +0200

    Inline LSP hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,81 +1,94 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"gopls",
-					"ts_ls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "gopls",
+          "ts_ls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						diagnostics = {
-							-- Get the language server to recognize the `vim` global
-							globals = { "vim" },
-						},
-						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
-							checkThirdParty = false, -- Disable third-party checking
-						},
-						telemetry = {
-							enable = false,
-						},
-						single_file_support = true,
-						autostart = true,
-					},
-				},
-				capabilities = capabilities,
-			})
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false, -- Disable third-party checking
+            },
+            telemetry = {
+              enable = false,
+            },
+            single_file_support = true,
+            autostart = true,
+          },
+        },
+        capabilities = capabilities,
+      })
 
-			vim.lsp.config("gopls", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("gopls", {
+        capabilities = capabilities,
+      })
 
-			vim.lsp.config("ts_ls", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
 
-			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("gopls")
-			vim.lsp.enable("ts_ls")
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("gopls")
+      vim.lsp.enable("ts_ls")
 
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = false,
-			})
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = false,
+      })
 
-			-- delete global default keymaps
-			vim.keymap.del({ "n", "v" }, "gra")
-			vim.keymap.del("n", "grr")
-			vim.keymap.del("n", "gri")
+      -- delete global default keymaps
+      vim.keymap.del({ "n", "v" }, "gra")
+      vim.keymap.del("n", "grr")
+      vim.keymap.del("n", "gri")
 
-			-- custom keymaps
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      -- custom keymaps
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {
+      hint_prefix = "",
+      doc_lines = 0,
+      toggle_key = "<C-x>",
+      -- floating_window = false,
+      -- hint_inline = function()
+      --   return true
+      -- end,
+    },
+  },
 }
